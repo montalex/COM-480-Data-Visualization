d53bd59d440f40f17206deb341177e3b7089fb44
Alexis Montavon <alexis.montavon@epfl.ch>
2017-11-21 18:55:36 +0100

    good zooming, cities, modifiy map color

--- World_Map/main.js
+++ World_Map/main.js
@@ -25,7 +25,7 @@ var options = select
 	.data(select_box)
 	.enter()
 	.append('option')
-	.text(function (d) { return d; });
+	.text(d => { return d; });
 let selectedMap = d3.select('select').property('value');
 
 let svg = d3.select("#container")
@@ -36,11 +36,10 @@ let svg = d3.select("#container")
 
 let selectedCountry = d3.select(null);
 
-//colormap for rating average
+//colormap for selected dropdown option
 const color = d3.scaleLog()
 	.range(["hsl(62,100%,90%)", "hsl(228,30%,20%)"])
 	.interpolate(d3.interpolateHcl);
-
 
 // Append Div for tooltip to SVG
 let div = d3.select("body")
@@ -48,16 +47,17 @@ let div = d3.select("body")
 	.attr("class", "tooltip")
 	.style("opacity", 0);
 
-d3.json("../Data/world-map.json", function(json) {
-	const countries = topojson.feature(json, json.objects.countries);
+// Countries variable get filled by the json
+let countries = null;
+
+d3.json("../Data/world-map.json", json => {
+	countries = topojson.feature(json, json.objects.countries);
 	// Adding Country name to json.
-	d3.csv("../Data/country-code.csv", function(data) {
+	d3.csv("../Data/country-code.csv", data => {
 		// Loop through each country data value in the .csv file
 		for(let i = 0; i < data.length; i++) {
-
-			// Country name
+			// Country code
 			let dataCountryCode = data[i].Code;
-
 			// Country name
 			let dataCountryName = data[i].Name;
 
@@ -73,25 +73,52 @@ d3.json("../Data/world-map.json", function(json) {
 			}
 		}
 	});
+	drawMap("../Data/ratebeer_dataset_all_with_loc.csv", countries);
+	drawCities("../Data/ratebeer_dataset_all_with_loc.csv");
+});
 
-	d3.csv("../Data/ratebeer_dataset_all_with_loc.csv", function(data) {
+/**
+ * [Draws map in the svg]
+ * @param  {[String]} data_path [Path to csv file]
+ * @param  {[topoJson]} countries [topoJson list of countries]
+ */
+function drawMap(data_path, countries) {
+	d3.csv(data_path, data => {
 		const fullDict = csvToBeerDict(data);
-		//Draw map & show tooltip with name when mouse over country
-		let map_window = d3.select("g").selectAll("path")
+		let selectedDict = "avgScore";
+		let selectedText = "Average Score: ";
+		switch(selectedMap) {
+			case "Alcool %":
+				selectedDict = "avgAlc";
+				selectedText = "Average Alcool %: ";
+				break;
+			case "Calory":
+				selectedDict = "avgCal";
+				selectedText = "Average Calories: ";
+				break;
+			default:
+				selectedDict = "avgScore";
+				selectedText = "Average Score: ";
+		}
+		//Draw map
+		d3.select("g").selectAll("path")
 			.data(countries.features)
 			.enter()
 			.append("path")
 			.attr("class", "boundary")
 			.attr("d", path)
-			.style("fill", function(d) {
+			.style("fill", d => {
 				//return color based on average
-				return color(fullDict["avgScore"][d.name]);
+				let colorValue = fullDict[selectedDict][d.name];
+				if(selectedMap === "Calory") {
+					colorValue = Math.max(colorValue / 100, 1.0);
+				}
+				return color(colorValue);
 			})
 			.on("click", function(d) {
 				document.getElementById("information").style.display = "block";
 				document.getElementById("countryName").innerHTML = "Country: " +  d.name;
-				document.getElementById("averageScore").innerHTML =  "Average Score: " + fullDict["avgScore"][d.name];
-
+				document.getElementById("averageScore").innerHTML =  selectedText + fullDict[selectedDict][d.name];
 				if(selectedCountry.node() === this) {
 					return zoomOutOnClick();
 				}
@@ -109,29 +136,55 @@ d3.json("../Data/world-map.json", function(json) {
 			  	svg.transition()
 			    	.duration(750)
 			      	.call( zoom.transform, d3.zoomIdentity.translate(translate[0],translate[1]).scale(scale));
+				//citiesLocs = citiesDict(data, d.name);
+				//drawCities(citiesLocs);
 			});
 	});
-});
+};
 
-//Draw cities on the map
-//d3.csv("cities.csv", function(error, data) {
-//    g.selectAll("circle")
-//    	.data(data)
-//        .enter()
-//        .append("a")
-//		.attr("xlink:href", function(d) {
-//					   return "https://www.google.com/search?q="+d.city;}
-//				  )
-//        .append("circle")
-//        .attr("cx", function(d) {
-//                return projection([d.lon, d.lat])[0];
-//        })
-//        .attr("cy", function(d) {
-//                return projection([d.lon, d.lat])[1];
-//        })
-//        .attr("r", 5)
-//        .style("fill", "red");
-//});
+/**
+ * [Draws the city on the map as circles]
+ * @param  {[type]} data_path [description]
+ * @return {[type]}           [description]
+ */
+function drawCities(data_path) {
+	d3.csv(data_path, data => {
+	    d3.select("g").selectAll("circle")
+	    	.data(data)
+	        .enter()
+	        .append("circle")
+	        .attr("cx", function(d) {
+	                return projection([d.lon, d.lat])[0];
+	        })
+	        .attr("cy", function(d) {
+	                return projection([d.lon, d.lat])[1];
+	        })
+	        .attr("r", 1)
+	        .style("fill", "darkblue")
+			.on("click", function(d) {
+				document.getElementById("cityName").innerHTML = "City: " +  d.city;
+			});
+	});
+};
+
+/**
+ * [Draws the city on the map as circles]
+ * @param  {[dictionary]} citiesDict [dictionary of type cityName : location]
+ */
+//function drawCities(citiesDict) {
+//	for(let key in citiesDict) {
+//		console.log([citiesDict[key][1], citiesDict[key][0]]);
+//		d3.select("g").selectAll("circle")
+//	        .append("circle")
+//	        .attr("cx", projection([citiesDict[key][1], citiesDict[key][0]])[0])
+//	        .attr("cy", projection([citiesDict[key][1], citiesDict[key][0]])[1])
+//	        .attr("r", 1)
+//	        .style("fill", "darkblue")
+//			.on("click", function(d) {
+//				document.getElementById("cityName").innerHTML = "City: " +  key;
+//			});
+//	}
+//};
 
 /**
  * [Create and order (based on score) dictionary of Country: [beer, score],
@@ -167,7 +220,6 @@ function csvToBeerDict(data) {
 		if(country.indexOf(" USA") > -1) {
 			country = "United States of America";
 		}
-
 		//Clean UK data
 		if(country === "England" || country === "Scotland" || country === "Wales" || country === "Northern Ireland") {
 			country = "United Kingdom";
@@ -187,10 +239,10 @@ function csvToBeerDict(data) {
 
 	// Ordering and getting average for each country.
 	for(let key in beerDict){
-		beerDict[key].sort(function(x, y){return y[1] - x[1]});
-		avgScore[key] = avgScore[key] / beerDict[key].length;
-		avgAlc[key] = avgAlc[key] / beerDict[key].length;
-		avgCal[key] = avgCal[key] / beerDict[key].length;
+		beerDict[key].sort((x, y) => {return y[1] - x[1]});
+		avgScore[key] = (avgScore[key] / beerDict[key].length).toFixed(2);
+		avgAlc[key] = (avgAlc[key] / beerDict[key].length).toFixed(2);
+		avgCal[key] = (avgCal[key] / beerDict[key].length).toFixed(2);
 	}
 
 	dictWithAvg["beerDict"] = beerDict;
@@ -200,12 +252,40 @@ function csvToBeerDict(data) {
 	return dictWithAvg;
 };
 
+/**
+ * [Returns every cities from the given country with localisation]
+ * @param  {[CSV]} data        [CSV file]
+ * @param  {[String]} countryName [The country to look for]
+ * @return {[dictionary]}             [A dictionary citieName : localisation]
+ */
+function citiesDict(data, countryName) {
+	let citiesDict = {};
+	for(let i = 0; i < data.length; i++) {
+		let country = data[i].country;
+		//Clean USA data
+		if(country.indexOf(" USA") > -1) {
+			country = "United States of America";
+		}
+		//Clean UK data
+		if(country === "England" || country === "Scotland" || country === "Wales" || country === "Northern Ireland") {
+			country = "United Kingdom";
+		}
+
+		if(country === countryName) {
+			citiesDict[data[i].city] = [data[i].lat, data[i].lon];
+		}
+	}
+	return citiesDict;
+};
+
+// Modify map when selection in dropdown is changed
 function changeMap() {
 	selectedMap = d3.select('select').property('value');
-	console.log(selectedMap);
-}
+	d3.select('#container').selectAll('path').remove();
+	drawMap("../Data/ratebeer_dataset_all_with_loc.csv", countries);
+};
 
-//Rescale when window size changes
+// Rescale when window size changes
 function sizeChange() {
 	d3.select("g").attr("transform", "scale(" + $("#container").width()/900 + ")");
 	$("svg").height($("#container").width()*0.6);
@@ -221,6 +301,7 @@ function zooming() {
 	d3.select("g").attr("transform", d3.event.transform); // updated for d3 v4
 };
 
+// Zooming out when reclicking on same country
 function zoomOutOnClick() {
 	selectedCountry.classed("active", false);
 	selectedCountry = d3.select(null);
